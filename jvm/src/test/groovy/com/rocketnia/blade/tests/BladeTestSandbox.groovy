1bb3f13b099971696f40683e78dcc0f7273dbf84
Ross Angle <rokitna@hotmail.com>
2010-05-06 00:29:45 -0700

    Added unit testing skeleton.

---
+++ jvm/src/test/groovy/com/rocketnia/blade/tests/BladeTestSandbox.groovy
@@ -0,0 +1,34 @@
+// BladeTestSandbox.groovy
+//
+// Copyright 2010 Ross Angle
+//
+// This file is part of JVM-Blade.
+//
+// JVM-Blade is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// JVM-Blade is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with JVM-Blade.  If not, see <http://www.gnu.org/licenses/>.
+
+
+package com.rocketnia.blade.tests
+
+
+println "Beginning BladeTestSandbox"
+
+def lines = []
+new InputStreamReader(
+	getClass().getResourceAsStream( "/resource.txt" ),
+	"UTF-8"
+).eachLine { lines.add it }
+
+println lines.inspect()
+
+println "Finishing BladeTestSandbox"
---
+++ jvm/src/test/groovy/com/rocketnia/blade/tests/BladeTests.groovy
@@ -0,0 +1,46 @@
+// BladeTests.groovy
+//
+// Copyright 2010 Ross Angle
+//
+// This file is part of JVM-Blade.
+//
+// JVM-Blade is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// JVM-Blade is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with JVM-Blade.  If not, see <http://www.gnu.org/licenses/>.
+
+
+package com.rocketnia.blade.tests
+
+
+class BladeTests extends GroovyTestCase
+{
+	void testTestResources()
+	{
+		def lines = []
+		
+		new InputStreamReader(
+			getClass().getResourceAsStream( "/resource.txt" ),
+			"UTF-8"
+		).eachLine { lines.add it }
+		
+		assertEquals lines, [
+			"Hello!",
+			"",
+			"I",
+			"am",
+			"a",
+			"resource.",
+			"",
+			"Have a nice day."
+		]
+	}
+}
